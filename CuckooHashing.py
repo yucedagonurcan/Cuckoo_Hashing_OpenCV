cfa0ae9201a3389d6eede15419fe402a13113553
oyucedag <onur@adastec.com>
2020-06-21 02:01:48 +0300

    initial commit

---
+++ CuckooHashing.py
@@ -0,0 +1,98 @@
+import cv2
+import random
+import numpy as np
+from Hash import HashCell, HashTable
+
+dummy = lambda x : 0
+
+WINDOW_NAME = 'Image'
+WIN_SIZE = [900, 1800, 3]
+NUM_CELLS = 10
+NUM_TABLES = 2
+MARGIN = 10
+
+def CheckNumExistsInHashTables(hash_tables, number):
+    results = []
+    for has_tab in hash_tables:
+        results.append(has_tab.CheckNumExists(number))
+    return any(results)
+
+def AddNumberToHashTables(hash_tables, number, exclude_hash_tables):
+    
+    if CheckNumExistsInHashTables(hash_tables, number):
+        print(f"{number} is already exists...")
+        return True
+    
+    search_hash_tables = [ x for x in list(range(len(hash_tables))) if x not in exclude_hash_tables]
+    if len(search_hash_tables) == 0:
+        search_hash_tables = list(range(len(hash_tables) - 1))
+        random.shuffle(search_hash_tables)
+        exclude_hash_tables = []
+
+    for current_try_hash_table in search_hash_tables:
+        
+        reinsert_number = hash_tables[current_try_hash_table].TryAddNumber(number, image, WINDOW_NAME)
+        if reinsert_number is not None:
+            exclude_hash_tables.append(current_try_hash_table)
+            return AddNumberToHashTables(hash_tables, reinsert_number, exclude_hash_tables)
+            
+        else: 
+            return True
+            
+def CreateHashTables(win_size, num_tables, num_cells, margin):
+    hash_tables = []
+    image = np.zeros(shape=win_size)
+
+    width_of_each_table = round((win_size[1]*0.8)/num_tables)
+    for i in range(num_tables):
+        
+        start_x_for_rects = int(win_size[1] * 0.1 + width_of_each_table * (i))
+        end_x_for_rects =  int(start_x_for_rects + width_of_each_table)
+        
+        start_y_for_rects = int(win_size[0] * 0.1)
+        end_y_for_rects = int(win_size[0] * 0.9)
+
+        h1 = HashTable(start_point=(start_x_for_rects, start_y_for_rects), end_point=(end_x_for_rects, end_y_for_rects), num_cells=num_cells, name=f"h_{i}")
+        h1.CalculateRectsPositions(margin=margin)
+        image = h1.DrawTable(image=image)
+        hash_tables.append(h1)
+    return image, hash_tables
+
+if __name__ == "__main__":
+
+    cv2.namedWindow(WINDOW_NAME)
+    # cv2.createTrackbar("Margin", WINDOW_NAME, 10, 100, dummy)
+    # cv2.setTrackbarMin("Margin", WINDOW_NAME, 10)
+    
+    # cv2.createTrackbar("NumCells", WINDOW_NAME, 10, 100, dummy)
+    # cv2.setTrackbarMin("NumCells", WINDOW_NAME, 1)
+    
+    # cv2.createTrackbar("NumTables", WINDOW_NAME, 1, 30, dummy)
+    # cv2.setTrackbarMin("NumTables", WINDOW_NAME, 1)
+    
+    cv2.createTrackbar("AddNumber", WINDOW_NAME, 1, 300, dummy)
+    cv2.setTrackbarMin("AddNumber", WINDOW_NAME, 0)
+    image, hash_tables = CreateHashTables(win_size=WIN_SIZE, num_tables=NUM_TABLES, num_cells=NUM_CELLS, margin=MARGIN)
+    
+    while(True):
+        
+        # margin = cv2.getTrackbarPos("Margin", WINDOW_NAME)
+        # num_cells = cv2.getTrackbarPos("NumCells", WINDOW_NAME)
+        # num_tables = cv2.getTrackbarPos("NumTables", WINDOW_NAME)
+        add_number = cv2.getTrackbarPos("AddNumber", WINDOW_NAME)
+        
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('p'):
+            
+            AddNumberToHashTables(hash_tables=hash_tables, number=add_number, exclude_hash_tables=[])
+            for table in hash_tables:
+                table.DrawTable(image=image)
+                
+        elif k == ord('r'):
+            image, hash_tables = CreateHashTables(win_size=WIN_SIZE, num_tables=NUM_TABLES, num_cells=NUM_CELLS, margin=MARGIN)
+        elif k == 27:
+            break
+        
+        
+        cv2.imshow(WINDOW_NAME,image)
+    cv2.destroyAllWindows()
---
+++ Hash.py
@@ -0,0 +1,118 @@
+
+import cv2
+import numpy as np
+from random import randint
+import time
+class HashTable():
+    
+    def __init__(self, start_point, end_point, num_cells, name):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.num_cells = num_cells
+        self.random_num = randint(4, 1000)
+        self.name = name
+        self.occupancy = 0.0
+        
+    def CalculateRectsPositions(self, margin=10):
+        start_x, start_y = self.start_point
+        end_x, end_y = self.end_point
+        
+        rects_height = end_y - start_y
+        rects_width = end_x - start_x
+        
+        height_for_each_block = round(rects_height/self.num_cells)
+        width_for_each_table = round(rects_width)
+        
+        y_s = np.linspace(start_y, end_y-height_for_each_block, dtype=int, num=self.num_cells).reshape(-1, 1)
+        x_s = np.array([round(start_x)]*y_s.size).reshape(-1, 1)
+
+        rects_pos = np.concatenate([x_s, y_s, x_s + width_for_each_table - margin, y_s + height_for_each_block - margin], axis=1)
+        self.cells = [HashCell(start_point=tuple(rect[:2]), end_point=tuple(rect[2:])) for rect in rects_pos] 
+    def CalculateOccupancy(self):
+        taken=0
+        for cell in self.cells:
+            if not cell.is_empty:
+                taken+=1
+        self.occupancy = round((taken/self.num_cells)*100, 2)
+    def DrawTable(self, image):
+        occupancy_font_scale = 1
+        self.CalculateOccupancy()
+        for cur_rect in self.cells:    
+            image = cv2.rectangle(image, cur_rect.start_point, cur_rect.end_point, cur_rect.color,  cur_rect.thickness)
+            width_rect, height_rect = np.array(cur_rect.end_point - np.array(cur_rect.start_point))
+            font_scale = np.min((width_rect, height_rect))/70
+
+            image = cv2.putText(image, str(cur_rect.number),
+                                (int(cur_rect.start_point[0] + width_rect*0.45),
+                                int(cur_rect.end_point[1] - height_rect*0.3) ), cv2.FONT_HERSHEY_SIMPLEX, font_scale, cur_rect.text_color)
+            
+        image = cv2.rectangle(image, (int(self.start_point[0] + width_rect*0.20), self.end_point[1] ),
+                                    (int(self.end_point[0] - width_rect*0.20), self.end_point[1] + 45), (21, 55, 55),  -1)
+
+        occupancy_width = int(self.end_point[0] - width_rect*0.20) - int(self.start_point[0] + width_rect*0.20)
+        occupancy_height = self.end_point[1] + 45 - self.end_point[1]
+
+        occupancy_font_scale = np.min((occupancy_width, occupancy_height))/110
+
+        width_of_table = (self.end_point[0] - self.start_point[0])
+        image = cv2.putText(image, str(self.occupancy) + '%',
+                            (int(self.start_point[0] + width_of_table*0.30), int(self.end_point[1] + 25)),
+                            cv2.FONT_HERSHEY_SIMPLEX, occupancy_font_scale, (0, 0, 0))
+        return image
+    def HashFunc(self, x):
+        
+        return (np.abs(hash(str(x))) * ((self.random_num)**2)) %self.num_cells 
+    
+    def CheckNumExists(self, number):
+        return self.cells[self.HashFunc(number)].number == number 
+        
+    def TryAddNumber(self, number, image, winname):
+        
+        print(f"- {number} is trying to added to {self.name}: ")
+        index_to_place = self.HashFunc(number)
+        # self.cells[index_to_place].color = (255, 255, 255)
+        
+        reinsert_number = None
+        
+        i = 0
+        while (True):
+            
+            self.cells[index_to_place].color =  (0, 20, 25)
+            self.cells[index_to_place].text_color =  (0, 0, 0)
+
+            cv2.imshow(winname, self.DrawTable(image))
+            k = cv2.waitKey(33) & 0xFF
+            if k == ord('p'):    
+                break;
+            i += 1
+        if self.cells[index_to_place].is_empty:
+            print(f"\t{index_to_place} is empty, so {number} will be added.")
+            self.cells[index_to_place].color = (55, 0, 0)
+            self.cells[index_to_place].text_color = (255, 255, 255)
+
+            self.cells[index_to_place].SetNumber(number = number)
+        else:
+            print(f"\t{index_to_place} is NOT empty, so we are re-inserting {self.cells[index_to_place].number} to add {number} in {self.name} hash table")
+            self.cells[index_to_place].color = (55, 0, 0)
+            self.cells[index_to_place].text_color = (255, 255, 255)
+
+            reinsert_number = self.cells[index_to_place].number
+            self.cells[index_to_place].SetNumber(number = number)
+            
+        return reinsert_number
+                
+class HashCell():
+    
+    def __init__(self, start_point, end_point, color=(55, 0, 0), text_color=(255, 255, 255), thickness=-1):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.color = color
+        self.text_color = text_color
+        self.thickness = thickness
+        self.is_empty = True
+        self.number = ""
+        
+        
+    def SetNumber(self, number=""):
+        self.number = number
+        self.is_empty = False
